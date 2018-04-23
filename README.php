9e6313a56ab49e610bf2439f1172e0adc37e3938
Andre Pratama <andre.pratama@profisien.co.id>
2018-04-23 15:10:58 +0700

    first commit

---
+++ README.php
@@ -0,0 +1,19 @@
+# README
+
+DEVELOPMENT PURPOSE
+
+## How to configure
+
+Open `keycloak-openid.php` edit the `get_properties` function
+
+## How to use
+
+Add this shortcode in secured pages
+```
+[keycloak_authenticate]
+```
+
+Add this shortcode in logout page
+```
+[keycloak_logout]
+```
